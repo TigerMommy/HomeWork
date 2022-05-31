0ebbe1ae0e33e48ff0ab193caf7733ca347556b9
TigerMommy <waste11@mail.ru>
2022-05-31 18:55:27 +0300

    second lesson

--- LevelOne/src/main/java/Main.java
+++ LevelOne/src/main/java/Main.java
@@ -9,13 +9,40 @@ public class Main {
 //        System.out.println(b2);
         System.out.println("Hello World!");
 
-        short s1 = -1300;  // decimal
-        short s2 = -0x514;  // hexadecimal
-        short s3 = -02424;  // octal
-        short s4 = -0b10100010100;  // binary
+//        short s1 = -1300;  // decimal
+//        short s2 = -0x514;  // hexadecimal
+//        short s3 = -02424;  // octal
+//        short s4 = -0b10100010100;  // binary
+//
+//        System.out.println(s2);
+//        System.out.println(s3);
+//        System.out.println(s4);
+//        int i1 = 0;
+//        int i2 = 0x0;
+//        int i3 = 00;
+//        int i4 = 0b0;
+//System.out.println(i4);
+//        long l1 = 123456789;
+//        long l2 = 0x75BCD15;
+//        long l3 = 0726746425;
+//        long l4 = 0b111010110111100110100010101;
+//        System.out.println(l4);
+//        float f1 = 2.5f;
+//        float f2 = 5.6f;
+//        System.out.println(f1);
+//        System.out.println(f2);
+//        double d1 = 33.22;
+//        double d2 = 22.33;
+//        System.out.println(d1);
+//        System.out.println(d2);
 
-        System.out.println(s2);
-        System.out.println(s3);
-        System.out.println(s4);
-    }
+
+        // %s - String, %d - digit, %f - float, double
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("Hello %d!!!\n", i);
+        }
+
+        //    /n   - перенос курсора на другую строку.
+        System.out.printf("My name is %s!!!\n", "Pasha");
+       }
 }
