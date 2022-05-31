37e0f8c787169fb6130708610fe710443d1dde63
TigerMommy <waste11@mail.ru>
2022-05-31 18:26:17 +0300

    second lesson

--- LevelOne/src/main/java/Main.java
+++ LevelOne/src/main/java/Main.java
@@ -8,5 +8,14 @@ public class Main {
 //        byte b2 = 0B1100;
 //        System.out.println(b2);
         System.out.println("Hello World!");
+
+        short s1 = -1300;  // decimal
+        short s2 = -0x514;  // hexadecimal
+        short s3 = -02424;  // octal
+        short s4 = -0b10100010100;  // binary
+
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
     }
 }
