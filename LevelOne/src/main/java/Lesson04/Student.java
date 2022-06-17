2eea1b05d09f5bd8dfce124b83430ce3fa71c3d2
TigerMommy <waste11@mail.ru>
2022-06-17 12:17:42 +0300

    second lesson

---
+++ LevelOne/src/main/java/Lesson04/Student.java
@@ -0,0 +1,17 @@
+package Lesson04;
+
+public class Student {
+    int id;
+    String name;
+    String surname;
+    int year;
+   double MidMath;
+   double MidEcon;
+    double  MidEng;
+    // сноска mid - middle указывает на среднее значение определнного предмета(математика,экономика и английский);
+
+}
+
+
+
+
---
+++ LevelOne/src/main/java/Lesson05/Methodtest1.java
@@ -0,0 +1,13 @@
+package Lesson05;
+
+public class Methodtest1 {
+    public static void main(String[] args) {
+
+    }
+    int Slozhi(int a, int b, int c){
+
+        int result = a+b+c;
+        return result;
+    }
+    }
+
